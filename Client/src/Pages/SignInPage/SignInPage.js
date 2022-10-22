52469ca1a8874283fb33bc7435b101e48385d67d
Sebastian Burman <sebastianburman@pop-os.localdomain>
2022-10-21 20:09:22 -0400

    Moved login container into a form and made fields required

--- Client/src/Pages/SignInPage/SignInPage.js
+++ Client/src/Pages/SignInPage/SignInPage.js
@@ -15,52 +15,53 @@ function SignInPage(props) {
   const [passwordInput, setPasswordInput] = useState("")
   const [loggingIn, setLoggingIn] = useState(false)
 
-  async function login() {
-      setLoggingIn(true)
-      if(formValidation()){
-        const response = await UserRequest.authenticate(usernameInput, passwordInput)
-        if (response !== undefined) {
-          const token = response.data
-          if (token === "User not found") {
-            setUsernameInput("")
-            setPasswordInput("")
-            alert("User not found")
-          }
-          else {
-            props.setToken(token)
-            props.setAuth(true)
-            localStorage.setItem("token", token)
-            navigate("/", { replace: true })
-          }
+  async function login(event) {
+    event.preventDefault()
+    setLoggingIn(true)
+    if (formValidation()) {
+      const response = await UserRequest.authenticate(usernameInput, passwordInput)
+      if (response !== undefined) {
+        const token = response.data
+        if (token === "User not found") {
+          setUsernameInput("")
+          setPasswordInput("")
+          alert("User not found")
+        }
+        else {
+          props.setToken(token)
+          props.setAuth(true)
+          localStorage.setItem("token", token)
+          navigate("/", { replace: true })
         }
       }
-      setLoggingIn(false)
+    }
+    setLoggingIn(false)
   }
-  function formValidation(){
+  function formValidation() {
     return true
   }
 
   return (
     <div className="signInPageCon">
-      {loggingIn ? <img src={loadingLogin}></img>:<div className="loginCon">
+      {loggingIn ? <img src={loadingLogin}></img> : <form  className = "loginCon"onSubmit={login}>
         <div className="logoNameCon">
           <img src={Logo}></img>
           <h1>TetraRoom</h1>
         </div>
-        <div className="inputCon">
-          <label>Username</label>
-          <input value={usernameInput} onChange={(e) => setUsernameInput(e.target.value)}></input>
-        </div>
-        <div className="inputCon">
-          <label>Password</label>
-          <input value={passwordInput} type="password" onChange={(e) => setPasswordInput(e.target.value)}></input>
-        </div>
-        <button className="loginButton" onClick={() => login()}>Login</button>
+          <div className="inputCon">
+            <label>Username</label>
+            <input value={usernameInput} onChange={(e) => setUsernameInput(e.target.value)} required></input>
+          </div>
+          <div className="inputCon">
+            <label>Password</label>
+            <input value={passwordInput} type="password" onChange={(e) => setPasswordInput(e.target.value)} required></input>
+          </div>
+          <input className="loginButton" type="submit"></input>
         <div className="signUpLinkTextCon">
           <p>Don't have an account</p>
           <Link to="/signup">Create an Account</Link>
         </div>
-      </div>}
+      </form>}
     </div>
   );
 }
